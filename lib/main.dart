8861966288c12940c151bff0d5aaa69464195cc8
ruben2490 <rubengmg90@gmail.com>
2025-06-18 17:59:35 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,19 @@
-// Main entry point
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gastos App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Gastos App')),
+        body: Center(child: Text('Bienvenido a la App de Gastos')),
+      ),
+    );
+  }
+}
